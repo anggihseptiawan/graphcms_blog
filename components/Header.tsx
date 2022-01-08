05e41010856725609b92826a6bfbb5f67854a113
Anggih Septiawan <albertanggih@gmail.com>
2022-01-08 14:57:15 +0700

    fix: category header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,26 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getCategories } from "../services";
 
 const Header = () => {
-  const categories = [
-    {
-      name: "React",
-      slug: "react",
-    },
-    {
-      name: "Next Js",
-      slug: "nextjs",
-    },
-  ];
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((categories) => setCategories(categories));
+  }, []);
 
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
-              GraphCMS
-            </span>
+            <span className="cursor-pointer font-bold text-4xl">GraphCMS</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories?.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md:float-right mr-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <span className="md:float-right mr-2 align-middle ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
