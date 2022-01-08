4697feefa03e7a87ae3bc69d9aaf8a602803b1e9
Anggih Septiawan <albertanggih@gmail.com>
2022-01-08 14:55:34 +0700

    feat: create category component

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getCategories } from "../services";
+
 const Categories = () => {
-  return <div>Categories</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((categories) => setCategories(categories));
+  }, []);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
+      {categories?.map((category) => (
+        <Link href={`/category/${category.slug}`} key={category.slug}>
+          <span className="cursor-pointer block pb-3 mb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
